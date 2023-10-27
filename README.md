bed4091aee2e8d92d7759ac4a87f05c8d34b602d
sd3g14 <stefandruc@gmail.com>
2023-10-27 12:00:31 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# How to use
+
+To install this for your neovim configuration
+
+```
+$ git clone git@https://github.com/sd3g14/nvim-config.git ~/.config/nvim/lua/custom
+```
+
+Then open up neovim and let everything install.
+
+Restart Neovim and install the treesitter syntax
+
+```
+:TSInstall cpp
+:TSInstall python
+```
---
+++ mappings.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    }
+  }
+}
+
+M.symbols = {
+  plugin = true,
+  n = {
+    ["<leader>s"] = {
+      "<cmd> SymbolsOutline <CR>",
+      "Toggle Symbols Outline"
+    },
+  }
+
+}
+
+return M
