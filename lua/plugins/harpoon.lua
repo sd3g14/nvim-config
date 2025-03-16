56cf6bd38293d1be77acfc18e526a087c74ca120
sd3g14 <stefandruc@gmail.com>
2025-03-16 20:29:27 +0000

    updated harpoon bindings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,42 +4,42 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     {
-      "<leader>ha",
+      "<leader>a",
       "<cmd>lua require('harpoon.mark').add_file()<CR>",
       desc = "Harpoon Mark File",
     },
     {
-      "<leader>hq",
+      "<leader>h",
       "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",
       desc = "Harpoon Toggle Quick Menu",
     },
     {
-      "<leader>hp",
-      "<cmd>lua require('harpoon.ui').nav_prev()<CR>",
-      desc = "Harpoon Go To Previous Mark",
-    },
-    {
-      "<leader>hn",
+      "<leader><Right>",
       "<cmd>lua require('harpoon.ui').nav_next()<CR>",
       desc = "Harpoon Go To Next Mark",
     },
     {
-      "<leader>h1",
+      "<leader><Left>",
+      "<cmd>lua require('harpoon.ui').nav_prev()<CR>",
+      desc = "Harpoon Go To Previous Mark",
+    },
+    {
+      "<leader>1",
       "<cmd>lua require('harpoon.ui').nav_file(1)<CR>",
       desc = "Harpoon Go To First Mark",
     },
     {
-      "<leader>h2",
+      "<leader>2",
       "<cmd>lua require('harpoon.ui').nav_file(2)<CR>",
       desc = "Harpoon Go To Second Mark",
     },
     {
-      "<leader>h3",
+      "<leader>3",
       "<cmd>lua require('harpoon.ui').nav_file(3)<CR>",
       desc = "Harpoon Go To Third Mark",
     },
     {
-      "<leader>h4",
+      "<leader>4",
       "<cmd>lua require('harpoon.ui').nav_file(4)<CR>",
       desc = "Harpoon Go To Fourth Mark",
     },
